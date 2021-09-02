764de8a245c763b5a74f370503464fcdde39438a
parammaan24 <parammaan24>
2021-09-03 00:01:34 +0530

    Add project files.

---
+++ FreshShop/Data/FreshShopContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreshShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreshShop.Data
+{
+    public class FreshShopContext : DbContext
+    {
+        public FreshShopContext (DbContextOptions<FreshShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vegetables> Vegetables { get; set; }
+
+        public DbSet<Seeds> Seeds { get; set; }
+
+        public DbSet<Fruits> Fruits { get; set; }
+    }
+}
